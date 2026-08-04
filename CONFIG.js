f2b1ae6e95bcedd043f958d759efb4ab9f522512
Cristian Camilo Montero <162826576+1000802680@users.noreply.github.com>
2026-08-03 21:05:31 -0500

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -3,6 +3,6 @@
    ============================================================ */
 
 window.APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://docs.google.com/spreadsheets/d/1zOttNV1TFjdqJU3hrOCYAda6nCSzExuQ6EOZfz7x6hk/edit?usp=sharing'
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyM3VqiqyFA4YCETT5nVDu-UKwB02jBw-K-nqZYIMNXN3fZzuEmGyGFhDUrv259BvNm/exec'
 };
 
